33b8fbb386cdc128af2fbc6a6c46966ccae5347f
acruseno <aldo.cruseno@gmail.com>
2019-02-14 03:09:22 -0300

    Final de video practica de formularios reactivos 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  formulario: FormGroup;
   title = 'formulario';
+
+  constructor(private frmbuilder: FormBuilder) {
+    this.formulario = this.frmbuilder.group({
+      nombre: ["Aldo", Validators.required],
+      apellido: ["Cruseño", Validators.required],
+      email: ["mail@servicio.com", [Validators.required, Validators.pattern('[^ @]*@[^ @]*')]]    
+    });
+  }
+
+  onSubmit(parametro){
+    console.log(parametro);
+  }
 }
